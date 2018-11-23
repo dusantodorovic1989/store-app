fce5b6668791580f52f5b1a9c4ff10d47da2a601
Dusan Todorovic <dusantodorovic1989@gmail.com>
2018-11-23 18:58:49 +0100

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    
+
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        
+        <router-link class="nav-link" to="custumers">Custumers</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="products">Products</router-link>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
